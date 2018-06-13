8bda4ef60034456cd90666ce4c0c8e886633cd2b
SebaArn <33929308+SebaArn@users.noreply.github.com>
2018-06-13 16:27:17 +0200

    reworked comments and functions, restructured

--- Plotter.py
+++ Plotter.py
@@ -1,10 +1,59 @@
-import numpy as np
+import numpy as np # used to handle numbers, data structures and mathematical functions
 import matplotlib.pyplot as plt  # MATLAB-like plotting
-import datetime
-import argparse
-import os.path
+import datetime # Used to convert our asci dates into unix-seconds
+import argparse # used to interpret parameters
 
-# This program creates an image that visualizes a given log file.
+
+# This program creates an image that visualizes a given log file in relation to a given quota.
+
+
+# translate_date_to_sec receives a date and returns the date in unix-time, if it's a valid date,
+# (i.e not "Unknown" otherwise returns -1)
+# If there is more invalid inputs possible in the log-system, this has to be expanded.
+
+plt.rcParams['figure.figsize'] = [6, 4]  # set global parameters, plotter initialisation
+
+tresholds = [0.7, 1.1, 1.5]  # If the usage this month is below this times the quota,
+colors = ['lightblue', "#008000", '#ffa500']  # the quota will be colored in
+maximum = '#ff0000'  # if the usage is above the highest times the quota, the quota will be colored in this color
+
+def translate_date_to_sec(ymdhms):
+    """
+:param ymdhms: the year-month-day-hour-minute-second data (datetime.datetime) to be translated into unix-time
+:return: the amount of seconds passed since the first of january 1970 00:00 UTC
+"""
+    x_ = str(ymdhms, 'utf-8')
+    if x_ == 'Unknown':
+        return -1
+    else:
+        temp_time = datetime.datetime.strptime(str(ymdhms, 'utf-8'), "%Y-%m-%d-%H-%M-%S")
+        return temp_time.timestamp()
+
+
+# separates the quotas into four categories
+
+
+def colorisation (value, comp):
+    """
+    :param value: The difference between the beginning of the Instance and the end, in Coreseconds
+    :param comp: the given Quota,
+    :return: A color based on the relationship of the two parameters, red for value >> comp, green
+    for value ~~ (roughly equal) comp, blue for comp >> value and orange for value > comp
+    """
+    if value/comp < tresholds[0]:
+        return colors[0]
+    if value / comp < tresholds[1]:
+        return colors[1]
+    if value / comp < tresholds[2]:
+        return colors[2]
+    else:
+        return maximum
+
+
+# creates quota-durations, current default value: 6 hours
+# suggested 30*24*60*60 for months, imprecise months, assumes 30 day per month
+# TODO: adapt seconds_per_instance depending on timerange
+seconds_per_instance = 6 * 60 * 60
 
 # Read parameter inputs three are expected; two filenames and one Number >0.
 # if the number is 0, an error is thrown, numbers below 0 will not throw an error, but
@@ -17,49 +66,22 @@ parameter = ap.parse_args()
 
 # parse parameters into values, divide the Quota into months from the yearly quota.
 # convert input-parameters into data to interpret
-plt.rcParams['figure.figsize'] = [6, 4]  # set global parameters, plotter initialisation
-target_file = ap.parse_args()
-yearly_quota = ap.parse_args().Quota
+target_file = parameter.Output[0]
+yearly_quota = parameter.Quota
 partial_quota = int(yearly_quota / 12)
+original = parameter.Source[0]
+
+
 data_type = np.dtype(
     [('JobID', '|S256'), ('ReqCPUS', 'i4'), ('ReqMem', '|S256'), ('ReqNodes', 'i4'), ('AllocNodes', 'i4'),
      ('AllocCPUS', 'i4'),
      ('NNodes', 'i4'), ('NCPUS', 'i4'), ('CPUTimeRAW', 'uint64'), ('ElapsedRaw', 'uint64'), ('Start', '|S256'),
      ('End', '|S256')])
-job_record = np.dtype([('ReqCPUS', 'i4'), ('ReqMem', 'i4')])
-original = parameter.Source[0]
-current_path = os.path.abspath(".")
 
-# copy and test may cause issues, as they use the currently active path and assume that both files actually exist
-copy = os.path.join(current_path, "copy.log-example")
-test = os.path.join(current_path, "testinput.log-example")
-# load the data from file with the given name, discarding most irrelevant sets of data.
+# loads the file specified in original/Source, noteworth are 'ReqCPUS', 'Start' and 'End' (3,26,27)
 Data = np.loadtxt(original, dtype=data_type, delimiter='|', skiprows=0, usecols=(1, 3, 4, 5, 6, 7, 8, 9, 12, 13, 26, 27))
-fin_dtype = np.dtype(
-    [('Corehours', 'uint64'), ('Endtime', 'S256')]
-)
-
-# translate_date_to_sec receives a date and returns the date in unix-time, if it's a valid date,
-# (i.e not "Unknown" otherwise returns -1)
-# If there is more invalid inputs possible in the log-system, this has to be expanded.
 
 
-def translate_date_to_sec(ymdhms):
-    """
-:param ymdhms: the year-month-day-hour-minute-second data (datetime.datetime) to be translated into unix-time
-:return: the amount of seconds passed since the first of january 1970 00:00 UTC
-"""
-    x_ = str(ymdhms, 'utf-8')
-    if x_ == 'Unknown':
-        return -1
-    else:
-        temptime = datetime.datetime.strptime(str(ymdhms, 'utf-8'), "%Y-%m-%d-%H-%M-%S")
-        return temptime.timestamp()
-
-
-# To find out the smallest value in a group, set a variable to a value higher than the highest
-#  value in the group and replace the variable's value with every lower value you find within the group
-# the same way the maximum value is determined.
 plot_array = (np.zeros((Data.size, 3)))
 x = 0
 time_accumulator = 0
@@ -69,19 +91,24 @@ x_end = datetime.datetime.strptime("2000-01-01-01-01-01", "%Y-%m-%d-%H-%M-%S")
 # Set y max to the estimated total amount of core-hours in a year for the Lichtenberg (for max,min computation)
 y_start1 = 1000000000000000000000000000000000
 y_end1 = 0
-# gathers the Cores used and multiplies with the time to generate Corehours.
+# gathers the Cores used and multiplies with the time (divided by 3600) to generate Corehours.
 for row in Data:
-    if translate_date_to_sec(row[11]) > 0:
-        end_t = datetime.datetime.strptime(str(row['End'], 'utf-8'), "%Y-%m-%d-%H-%M-%S")
-        start_t = datetime.datetime.strptime(str(row['Start'], 'utf-8'), "%Y-%m-%d-%H-%M-%S")
+    if translate_date_to_sec(row[11]) > 0:  # row[11] contains the end
+        end_t = datetime.datetime.strptime(str(row['End'], 'utf-8'), "%Y-%m-%d-%H-%M-%S")  # converts the string into a
+        # datetime construct to interpret the endtime
+        start_t = datetime.datetime.strptime(str(row['Start'], 'utf-8'), "%Y-%m-%d-%H-%M-%S")  # converts the string
+        # into a datetime to interpret the starttime
         x_start = min(end_t, x_start)
         x_end = max(end_t, x_end)
-        if (end_t - start_t).seconds < 1:
-            continue
-        y_start1 = min(y_start1, (row['AllocCPUS'] * (end_t - start_t).seconds))
+        if (end_t - start_t).seconds < 1: # for an invalid endtime (still running) or too short a process,
+            continue  # skip that set of data
+            # To find out the smallest value in a group, set a variable to a value higher than the highest
+            #  value in the group and replace the variable's value with every lower value you find within the group
+            # the same way the maximum value is determined.
+        y_start1 = min(y_start1, (row['AllocCPUS'] * (end_t - start_t).seconds)/3600)  # calculate the lowest
         y_end1 = max(y_end1, (row['AllocCPUS'] * (end_t - start_t).seconds) / 3600)
         plot_array[x, 0] = end_t.timestamp()
-        plot_array[x, 1] = (row['AllocCPUS'] * (end_t - start_t).seconds / 3600)
+        plot_array[x, 1] = (row['AllocCPUS'] * (end_t - start_t).seconds / 3600)  # writes the duration into the array
         x = x + 1
 
 # creates a cutoff after the array runs out of values and sorts it.
@@ -96,31 +123,23 @@ for index in range(1, plot_array.shape[0]):
     plot_array[index, 2] = plot_array[index, 1] + plot_array[index - 1, 2]
     y_end2 = max(y_end2, plot_array[index, 2])
 
-# reading in testdata
-second_data = np.loadtxt(test, dtype='S256', delimiter='|', skiprows=0, usecols=(1, 14, 26, 27))
 
-# test outputs, clears writingfile, then adds the three datapoints from test data for each row
-file = open("writingfile.txt", "w")
-file.close()
-file = open("writingfile.txt", "a")
-z = 0
-for row in second_data:
-    file.write(second_data[z][0].decode('UTF-8') + " " + second_data[z][2].decode('UTF-8') + " " +
-               second_data[z][1].decode('UTF-8') + "\n")
-    z = z + 1
-file.close()
 
 # splitting plot_array into x and y values to easier visualize them.
 tmp_x = [(datetime.datetime.fromtimestamp(i)) for i in plot_array[:, 0]]
 tmp_y = plot_array[:, 2]
 tmp_array = plot_array[:, (0, 2)]
 
-# creates quota-durations, current default value: 6 hours
-# suggested 30*24*60*60 for months, imprecise months, assumes 30 day per month
-#TODO: adapt seconds_per_instance depending on timerange
-seconds_per_instance = 6 * 60 * 60
+
 
 # splits the graph into intervals and creates three values for each instance, to visualise quotas
+# ___b-----c
+# ___|______
+# ___|______
+# ___a______
+# The three points a, b and c define each "L"
+# the coordinates for each L are saved in  tmp_x2 and tmp_y2
+
 number_of_instances = ((x_end.timestamp() - x_start.timestamp()) / seconds_per_instance) + 1
 tmp_x2 = np.arange(x_start.timestamp(), x_end.timestamp(), seconds_per_instance)
 tmp_x2 = np.repeat(tmp_x2, 3)
@@ -147,29 +166,6 @@ for x2 in range(0, np.size(tmp_x2)):
     tmp_x3[x2] = (datetime.datetime.fromtimestamp(tmp_x2[x2]))
 tmp_x3 = tmp_x3[0:int(number_of_instances) * 3:1]
 
-# separates the quotas into four categories
-
-
-def colorisation (value, comp):
-    """
-    :param value: The difference between the beginning of the Instance and the end, in Coreseconds
-    :param comp: the given Quota,
-    :return: A color based on the relationship of the two parameters, red for value >> comp, green
-    for value ~~ (roughly equal) comp, blue for comp >> value and orange for value > comp
-    """
-    if value/comp < 0.7:
-        return 'lightblue'
-    if value / comp < 1.1:
-        return "#008000"
-    if value / comp < 1.3:
-        return '#ffa500'
-    else:
-        return '#ff0000'
-
-
-# plotting the main graph
-plt.plot(tmp_x, tmp_y, 'black')
-# adds an additional unwanted line along the x-axis
 
 # determines the color via colorisation and then plots three points, stops before the last interval to draw
 for iterator in range(0, int(number_of_instances - 1)):
@@ -177,11 +173,15 @@ for iterator in range(0, int(number_of_instances - 1)):
     plt.plot([tmp_x3[iterator * 3], tmp_x3[iterator * 3 + 1], tmp_x3[iterator * 3 + 2]], [tmp_y2[iterator * 3], tmp_y2[iterator * 3 + 1], tmp_y2[iterator * 3 + 2]], col)
 
 # determines the last interval's color and draws it (uses the highest
-# recorded value as the end value of the ongoing timespan)
+# recorded value as the end value of the ongoing timespan
 col = colorisation(np.max(tmp_y)-tmp_y2[-3], tmp_y2[-1] - tmp_y2[-3])
 plt.plot([tmp_x3[-3], tmp_x3[-2], tmp_x3[-1]], [tmp_y2[-3], tmp_y2[-2], np.max(tmp_y2[-1])], col)
 
 axis = plt.gca()
+
+# plotting the main graph
+plt.plot(tmp_x, tmp_y, 'black')
+# adds an additional unwanted line along the x-axis
 
 # sets the visual borders for the graphs, area of occurring values (main graph) +- 5%
 temp_timestamp1 = x_start.timestamp()
@@ -194,11 +194,11 @@ axis.set_ylim([y_start2 - (0.05 * y_end2), y_end2 * 1.05])
 plt.grid(True)
 
 # labels the two axis
-plt.ylabel('Corehours')
-plt.xlabel('Enddate of Process')
+plt.ylabel('cores * hours')
+plt.xlabel('enddate of process (date, time)')
 manager = plt.get_current_fig_manager()
 
 # saves the graph as a file under the given name
 fig = plt.gcf()
 fig.set_size_inches((11, 8.5), forward=False)
-fig.savefig(target_file.Output[0], dpi=500)
+fig.savefig(target_file, dpi=500)
