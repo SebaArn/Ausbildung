671f2bc98fbfcbf5f5a6e8532eed570dac9d9a1c
SebaArn <sebastian.arncken@sc.tu-darmstadt.de>
2018-08-21 11:05:50 +0200

    implemented measures, other features. started commenting

--- reader.py
+++ reader.py
@@ -10,47 +10,72 @@ import matplotlib.patches as mpatches
 import re
 
 
-def fun_log(a, b, p, c):
+def fun_log(a, b, p, c):  # Implementation of a logarithmic function.
     # function = a+b*log2(p)
     return a + b * log2(p, c)
 
 
-def fun_poly(a, b, p):
+def fun_poly(a, b, p):  # Implementation of a polynomial function.
     return a+b*p
 
 
-def fun_pot(a, b, p, c):
+def fun_pot(a, b, p, c):  # Implementation of a potential function.
     return a + b* p**c
 
 
-def log2(x, y):
+def log2(x, y):  # logarithm to the base of 2^x
     return math.log(x, int(2**y))
 
 
 parser = argparse.ArgumentParser()
 # Creating different parameters to allow the user to specify, what data is to be evaluated.
 parser.add_argument('-name', dest='Name', default="", type=str, nargs=1)
+parser.add_argument('-o', dest='Output', default="", type=str, nargs=1)
 parser.add_argument("-start", dest="Start", type=int, default=4, nargs=1)
 parser.add_argument("-end", dest="End", default=64, type=int, nargs=1)
+parser.add_argument("-f", dest="Focus", type=int, nargs='*')
 parameter = parser.parse_args()
 print(parameter)
+
+deviationbar = False   # Determines if the deviation-bar is displayed.
+
 if not(parameter.Start and parameter.End and parameter.Name):
     sys.stderr.write("needs three parameters")
     sys.exit()
+fexists = False
+#eval = "Runtime"
 try:
     left = parameter.Start[0]
     right = parameter.End[0]
     filename = parameter.Name[0]
+    out = parameter.Output[0]
+    if parameter.Focus:
+        print(len(parameter.Focus))
+        print("focus=",parameter.Focus[0])
+        if len(parameter.Focus) > 0:
+            focus = parameter.Focus[0]
+            fexists = True
+    else:
+        print("no focus")
+        fexists = False
+
 except:
     sys.stderr.write("error reading file, aquiring left and right border")
     sys.exit()
-if left < 4:
-    print("a left border < 4 make no sense, replacing it with 4.")
-    left = 4
+if left < 1:
+    print("a left border < 1 make no sense, replacing it with 1.")
+    start = 1
+else:
+    start = left
 file_object = open(filename, 'r')
 lines = file_object.readlines()
 testinput = "0"
+measures = []
 for i in lines:
+    if "metric:" in i:
+        eval = i[9:-1]
+    if "Mean" in i:
+        measures.append(i)
     if "model: " in i:
         testinput = str(i)
 testinput = testinput.strip()
@@ -58,9 +83,8 @@ testinput = testinput.strip()
 # os.makedirs("thisshouldnotexist")
 # testinput = "model: 0+5*p"
 # testinput = "3.94994+0.00166031*(p^0.5)"
-# testinput  = "model: 0+9.99998e-07*(p^2)"
-print(testinput)
-print(testinput)
+# testinput  = "model: 0+1*(p^2)"
+# print(testinput)
 results = []
 a = 0
 b = 1
@@ -73,7 +97,6 @@ if "log" in reading and not mode:
     print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
     print("b", b)
-    print(reading.split("^")[1])
     c = float(reading.split("^")[1].split("(")[0] )
     print("c", c)
     mode = 1
@@ -90,38 +113,84 @@ if "+" in reading and not mode:
     print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
     print("b", b)
-    # c = 2**math.log(b,2)
     mode = 3
 if "p" not in reading and not mode:
     a = float(reading)
     b = 0
     mode = 4
-start = 2**int(math.log(left, 2))
-end = 2**int(math.log(right, 2))
-if end < start + 2:
-    print("end point needs to be higher than starting point, replacing it with +2")
-    end = start + 2
+start = 2**int(math.log(start, 2))
+end = 2**int(math.ceil(math.log(right, 2)))
+if end < start + 1:
+    print("end point needs to be higher than starting point, replacing it with the next 2^x")
+    end = 2**int(math.log(right, 2)+1)
 # modes:
-# 0 means log and poly
-# 1 means only poly
-# 2 means constant/literal
+# 0 means none (yet)
+# 1 means log
+# 2 means potential
+# 3 means  poly
+# 4 means constant/literal
+
 if mode == 1:
-    for p in range(start, end):
+    for p in range(start, (end+1)):
         results.append(fun_log(a, b, p, c))
+        #funtodraw = fun_log()
 if mode == 2:
-    for p in range(start, end):
+    for p in range(start, (end+1)):
         results.append(fun_pot(a, b, p, c))
+        #funtodraw = fun_pot()
 if mode == 3:
-    for p in range(start, end):
+    for p in range(start, end+1):
         results.append(fun_poly(a, b, p))
+        #funtodraw = fun_poly()
 if mode == 4:
-    for p in range(start, end):
+    for p in range(start, end+1):
         results.append(a)
-plt.plot(results)
+        #funtodraw = a
+plt.plot(range(start, end+1), results, color='grey', alpha=0.7)
+xval = []
+means = []
+for i in range(len(measures)):
+    xval.append(float(measures[i].split()[0]))
+    means.append(float(measures[i].split()[2]))
+axis = plt.gca()
+axis.set_xlim(start - 0.05*end, end + 0.05*end )
+axis.set_ylim(0, max(results)*1.05)
 plt.grid(True)
-# print(conv[9:-1])
-# print(a)
-print("given range:",left, right)
-print("used range:",start, end)
+print("given range:", left, "<->", right)
+print("used range:", start, "<->", end)
+
+plt.ylabel("run"+eval.lower()+" of computation")
+plt.xlabel('# of cores the program runs on')
+grey_patch = mpatches.Patch(color='grey', alpha=0.7, label='Run'+eval.lower()+' of computation with given cores')
+teal_patch = mpatches.Patch(color='#00e5e5', alpha=0.97, label='Measurements (mean)')
+plt.legend(handles=[grey_patch, teal_patch])
+manager = plt.get_current_fig_manager()
+
+t= ""
+f = []
+x2 = []
+if fexists:
+    #print("fexists")
+    for i in range(max(int(focus-0.05*(end-start)),int(start)),min(int(focus+0.05*(end-start)), end)):
+        # print(i)
+        f.append(results[i-start])
+        x2.append(i+1)
+        #t+=str(i)
+
+plt.plot(x2, f, linewidth=3,color='red', alpha=0.8)
+for i in range(len(xval)):
+    plt.plot([xval[i]], [means[i]], marker='o', markersize=3, color='#00e5e5', alpha=0.85)
+    if deviationbar:
+        plt.errorbar(xval[i], means[i], 0.05*(max(results)-results[0]), color='orange', alpha=0.9, capsize=3)
+
+
 plt.draw()
-plt.show()
+
+# plt.show()
+fig = plt.gcf()
+fig.set_size_inches((11, 8.5), forward=False)
+fig.savefig(out, dpi=500)
+print(means)
+print(xval)
+print(testinput)
+print(t)
