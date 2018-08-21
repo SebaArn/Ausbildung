7349d9072016fb6f3e8b942b77b8b68c4e8516c2
SebaArn <sebastian.arncken@sc.tu-darmstadt.de>
2018-08-21 15:11:27 +0200

    smoothed the function in low ranges

--- reader.py
+++ reader.py
@@ -35,12 +35,12 @@ parser.add_argument("-start", dest="Start", type=int, default=4, nargs=1)
 parser.add_argument("-end", dest="End", default=64, type=int, nargs=1)
 parser.add_argument("-f", dest="Focus", type=int, nargs='*')
 parameter = parser.parse_args()
-print(parameter)
+# print(parameter)
 
 deviationbar = False   # Determines if the deviation-bar is displayed.
 
-if not(parameter.Start and parameter.End and parameter.Name):
-    sys.stderr.write("needs three parameters")
+if not(parameter.Start and parameter.End and parameter.Name and parameter.Output):  # Invalid inputs
+    sys.stderr.write("needs a source, an output file, a start and an endpoint.")
     sys.exit()
 fexists = False
 #eval = "Runtime"
@@ -50,7 +50,7 @@ try:
     filename = parameter.Name[0]
     out = parameter.Output[0]
     if parameter.Focus:
-        print(len(parameter.Focus))
+        #print(len(parameter.Focus))
         print("focus=",parameter.Focus[0])
         if len(parameter.Focus) > 0:
             focus = parameter.Focus[0]
@@ -79,8 +79,6 @@ for i in lines:
     if "model: " in i:
         testinput = str(i)
 testinput = testinput.strip()
-# newpath = r'C:\thisshouldntexist'
-# os.makedirs("thisshouldnotexist")
 # testinput = "model: 0+5*p"
 # testinput = "3.94994+0.00166031*(p^0.5)"
 # testinput  = "model: 0+1*(p^2)"
@@ -94,25 +92,25 @@ reading = testinput.replace("model: ", "")
 # polynomial, logarithm, literal
 if "log" in reading and not mode:
     a = float(reading.split("+")[0])
-    print("a", a)
+    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    print("b", b)
+    #   print("b", b)
     c = float(reading.split("^")[1].split("(")[0] )
-    print("c", c)
+    #   print("c", c)
     mode = 1
 if "p^" in reading and not mode:
     a = float(reading.split("+")[0])
-    print("a", a)
+    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    print("b", b)
+    #   print("b", b)
     c = float(reading.split("^")[1].split(")")[0] )
-    print("c", c)
+    #   print("c", c)
     mode = 2
 if "+" in reading and not mode:
     a = float(reading.split("+")[0])
-    print("a", a)
+    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    print("b", b)
+    #   print("b", b)
     mode = 3
 if "p" not in reading and not mode:
     a = float(reading)
@@ -129,24 +127,28 @@ if end < start + 1:
 # 2 means potential
 # 3 means  poly
 # 4 means constant/literal
-
+x = []
 if mode == 1:
-    for p in range(start, (end+1)):
+    for p in np.arange(start, (end+1),0.1):
         results.append(fun_log(a, b, p, c))
+        x.append(p)
         #funtodraw = fun_log()
 if mode == 2:
-    for p in range(start, (end+1)):
+    for p in np.arange(start, (end+1),0.1):
         results.append(fun_pot(a, b, p, c))
+        x.append(p)
         #funtodraw = fun_pot()
 if mode == 3:
-    for p in range(start, end+1):
+    for p in np.arange(start, (end+1),0.1):
         results.append(fun_poly(a, b, p))
+        x.append(p)
         #funtodraw = fun_poly()
 if mode == 4:
-    for p in range(start, end+1):
+    for p in np.arange(start, (end+1),0.1):
         results.append(a)
+        x.append(p)
         #funtodraw = a
-plt.plot(range(start, end+1), results, color='grey', alpha=0.7)
+plt.plot(x, results, color='grey', alpha=0.7)
 xval = []
 means = []
 for i in range(len(measures)):
@@ -159,6 +161,8 @@ plt.grid(True)
 print("given range:", left, "<->", right)
 print("used range:", start, "<->", end)
 
+
+# Labeling the Axis and creating Legend
 plt.ylabel("run"+eval.lower()+" of computation")
 plt.xlabel('# of cores the program runs on')
 grey_patch = mpatches.Patch(color='grey', alpha=0.7, label='Run'+eval.lower()+' of computation with given cores')
@@ -166,18 +170,24 @@ teal_patch = mpatches.Patch(color='#00e5e5', alpha=0.97, label='Measurements (me
 plt.legend(handles=[grey_patch, teal_patch])
 manager = plt.get_current_fig_manager()
 
-t= ""
+
+# f: y-axis values
 f = []
+
+# x2: X-axis values within the specified range
 x2 = []
+
+# Copying values for specified range
 if fexists:
     #print("fexists")
-    for i in range(max(int(focus-0.05*(end-start)),int(start)),min(int(focus+0.05*(end-start)), end)):
+    for i in np.arange(max(int(focus-0.05*(end-start)),int(start)),min(int(focus+0.05*(end-start)), end),0.1):
         # print(i)
-        f.append(results[i-start])
+        f.append(results[int((i-start)*10)])
         x2.append(i+1)
         #t+=str(i)
 
-plt.plot(x2, f, linewidth=3,color='red', alpha=0.8)
+# Drawing marker in specified range
+plt.plot(x2, f, linewidth=3, color='red', alpha=0.8)
 for i in range(len(xval)):
     plt.plot([xval[i]], [means[i]], marker='o', markersize=3, color='#00e5e5', alpha=0.85)
     if deviationbar:
@@ -185,12 +195,9 @@ for i in range(len(xval)):
 
 
 plt.draw()
-
-# plt.show()
 fig = plt.gcf()
 fig.set_size_inches((11, 8.5), forward=False)
 fig.savefig(out, dpi=500)
 print(means)
 print(xval)
 print(testinput)
-print(t)
