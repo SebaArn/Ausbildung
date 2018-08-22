a8a3c5722ca5ced355a9d20b230502c74f19ff1a
SebaArn <sebastian.arncken@sc.tu-darmstadt.de>
2018-08-22 12:02:33 +0200

    fixed issue in reader.py focus area

--- reader.py
+++ reader.py
@@ -11,7 +11,6 @@ import re
 
 
 def fun_log(a, b, p, c):  # Implementation of a logarithmic function.
-    # function = a+b*log2(p)
     return a + b * log2(p, c)
 
 
@@ -33,32 +32,29 @@ parser.add_argument('-name', dest='Name', default="", type=str, nargs=1)
 parser.add_argument('-o', dest='Output', default="", type=str, nargs=1)
 parser.add_argument("-start", dest="Start", type=int, default=4, nargs=1)
 parser.add_argument("-end", dest="End", default=64, type=int, nargs=1)
-parser.add_argument("-f", dest="Focus", type=int, nargs='*')
+parser.add_argument("-f", dest="Focus", type=float, nargs='*')
 parameter = parser.parse_args()
-# print(parameter)
 
 deviationbar = False   # Determines if the deviation-bar is displayed.
 
 if not(parameter.Start and parameter.End and parameter.Name and parameter.Output):  # Invalid inputs
     sys.stderr.write("needs a source, an output file, a start and an endpoint.")
     sys.exit()
-fexists = False
-#eval = "Runtime"
+f_exists = False
+
 try:
     left = parameter.Start[0]
     right = parameter.End[0]
     filename = parameter.Name[0]
     out = parameter.Output[0]
     if parameter.Focus:
-        #print(len(parameter.Focus))
-        print("focus=",parameter.Focus[0])
+        print("focus=", parameter.Focus[0])
         if len(parameter.Focus) > 0:
             focus = parameter.Focus[0]
-            fexists = True
+            f_exists = True
     else:
         print("no focus")
-        fexists = False
-
+        f_exists = False
 except:
     sys.stderr.write("error reading file, aquiring left and right border")
     sys.exit()
@@ -79,10 +75,6 @@ for i in lines:
     if "model: " in i:
         testinput = str(i)
 testinput = testinput.strip()
-# testinput = "model: 0+5*p"
-# testinput = "3.94994+0.00166031*(p^0.5)"
-# testinput  = "model: 0+1*(p^2)"
-# print(testinput)
 results = []
 a = 0
 b = 1
@@ -92,25 +84,17 @@ reading = testinput.replace("model: ", "")
 # polynomial, logarithm, literal
 if "log" in reading and not mode:
     a = float(reading.split("+")[0])
-    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    #   print("b", b)
-    c = float(reading.split("^")[1].split("(")[0] )
-    #   print("c", c)
+    c = float(reading.split("^")[1].split("(")[0])
     mode = 1
 if "p^" in reading and not mode:
     a = float(reading.split("+")[0])
-    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    #   print("b", b)
-    c = float(reading.split("^")[1].split(")")[0] )
-    #   print("c", c)
+    c = float(reading.split("^")[1].split(")")[0])
     mode = 2
 if "+" in reading and not mode:
     a = float(reading.split("+")[0])
-    #   print("a", a)
     b = float(reading.split("+")[1].split("*")[0])
-    #   print("b", b)
     mode = 3
 if "p" not in reading and not mode:
     a = float(reading)
@@ -121,33 +105,32 @@ end = 2**int(math.ceil(math.log(right, 2)))
 if end < start + 1:
     print("end point needs to be higher than starting point, replacing it with the next 2^x")
     end = 2**int(math.log(right, 2)+1)
-# modes:
+
+# mode: determines, which type of function is to be called
 # 0 means none (yet)
 # 1 means log
 # 2 means potential
 # 3 means  poly
 # 4 means constant/literal
+
+# creating y-values with a function depending on which mode was selected.
 x = []
 if mode == 1:
     for p in np.arange(start, (end+1),0.1):
         results.append(fun_log(a, b, p, c))
         x.append(p)
-        #funtodraw = fun_log()
 if mode == 2:
     for p in np.arange(start, (end+1),0.1):
         results.append(fun_pot(a, b, p, c))
         x.append(p)
-        #funtodraw = fun_pot()
 if mode == 3:
     for p in np.arange(start, (end+1),0.1):
         results.append(fun_poly(a, b, p))
         x.append(p)
-        #funtodraw = fun_poly()
 if mode == 4:
     for p in np.arange(start, (end+1),0.1):
         results.append(a)
         x.append(p)
-        #funtodraw = a
 plt.plot(x, results, color='grey', alpha=0.7)
 xval = []
 means = []
@@ -161,7 +144,6 @@ plt.grid(True)
 print("given range:", left, "<->", right)
 print("used range:", start, "<->", end)
 
-
 # Labeling the Axis and creating Legend
 plt.ylabel("run"+eval.lower()+" of computation")
 plt.xlabel('# of cores the program runs on')
@@ -170,7 +152,6 @@ teal_patch = mpatches.Patch(color='#00e5e5', alpha=0.97, label='Measurements (me
 plt.legend(handles=[grey_patch, teal_patch])
 manager = plt.get_current_fig_manager()
 
-
 # f: y-axis values
 f = []
 
@@ -178,13 +159,10 @@ f = []
 x2 = []
 
 # Copying values for specified range
-if fexists:
-    #print("fexists")
-    for i in np.arange(max(int(focus-0.05*(end-start)),int(start)),min(int(focus+0.05*(end-start)), end),0.1):
-        # print(i)
+if f_exists:
+    for i in np.arange(max(focus-0.05*(end-start), int(start)), min(focus+0.05*(end-start), end)+0.1, 0.1):
         f.append(results[int((i-start)*10)])
-        x2.append(i+1)
-        #t+=str(i)
+        x2.append(x[int((i-start)*10)])
 
 # Drawing marker in specified range
 plt.plot(x2, f, linewidth=3, color='red', alpha=0.8)
@@ -193,11 +171,13 @@ for i in range(len(xval)):
     if deviationbar:
         plt.errorbar(xval[i], means[i], 0.05*(max(results)-results[0]), color='orange', alpha=0.9, capsize=3)
 
-
 plt.draw()
 fig = plt.gcf()
+# Saving the image to a file.
 fig.set_size_inches((11, 8.5), forward=False)
 fig.savefig(out, dpi=500)
-print(means)
-print(xval)
-print(testinput)
+# print(f)
+# print(x2)
+# print(means)
+# print(xval)
+# print(testinput)
